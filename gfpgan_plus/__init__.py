b32a71d1f0af9c3cae01b759b4e51c23526d9fc7
Nguyễn Viết Thành Đạt <v.datnvt2@vinai.io>
2024-08-21 00:18:17 +0700

    UPDATE init import

--- gfpgan_plus/__init__.py
+++ gfpgan_plus/__init__.py
@@ -1,5 +1,5 @@
-from .generator import GFPGANv1Clean, StyleGAN2GeneratorCSFT
-from .module import StyleGAN2GeneratorClean, ResBlock, ModulatedConv2d, EqualLinear, EqualConv2d
-from .ops.fused_act import FusedLeakyReLU, ScaledLeakyReLU
-from .ops.upfirdn2d import upfirdn2d
+from .model.generator import GFPGANv1Clean, StyleGAN2GeneratorCSFT
+from .model.module import StyleGAN2GeneratorClean, ResBlock, ModulatedConv2d, EqualLinear, EqualConv2d
+from .model.ops.fused_act import FusedLeakyReLU, ScaledLeakyReLU
+from .model.ops.upfirdn2d import upfirdn2d
 from .gfpganer_plus import GFPGANerPlus
